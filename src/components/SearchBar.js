9954ea2fdca7d19a824fc123b18233298d105a17
Sean H <sharper1006@hotmail.com>
2023-02-12 19:47:01 -0500

    commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className='search-bar'>
+      <input type='search' placeholder='Search Employees here...'/>
+    </div>
+  )
+}
+
+export default SearchBar
